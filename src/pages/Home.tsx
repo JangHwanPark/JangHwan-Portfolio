bab2f291c43313c54accd31a3eff256e2c1835de
JangHwan <wkd5878@gmail.com>
2025-02-24 11:47:10 +0900

    :memo: note - 페이지 내 워딩 작성

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,6 +2,7 @@ const Home = () => {
   return (
     <section className='w-full h-screen flex items-center justify-center'>
       <header className='font-bold leading-8'>
+        <p className='text-lg'>#프론트엔드 #협업 #팀워크 #사용자 중심</p>
         <h1 className='mb-8 text-3xl'>데이터를 조각하는 프론트엔드 개발자 박장환 입니다.</h1>
         <h2 className='text-xl'>저는 팀워크와 협업을 중시하는 신입 프론트엔드 개발자입니다.<br/>
           사용자 중심의 웹 애플리케이션을 개발하기 위해 팀과 함께 협력하며 성장하고 있습니다.<br/>
