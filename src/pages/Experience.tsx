3cb9229c51757e5ffe350b43f1120deea24f02cf
JangHwan <wkd5878@gmail.com>
2025-03-04 17:41:53 +0900

    :art: chore - Experience 반응형 작업 (1024px - lg)

--- src/pages/Experience.tsx
+++ src/pages/Experience.tsx
@@ -74,22 +74,26 @@ const Experience = () => {
 
           {/* 경력 항목 */}
           {experienceData.map((experience, index) => (
-            <div key={index} className="flex justify-between mb-[50px] flex-col md:gap-2 lg:gap-14 md:mb-[70px] lg:flex-row">
-              <div className="flex flex-row  justify-between gap-[50px] lg:w-[40%] lg:pl-[10%]">
-                {/* 경력 콘텐츠 */}
+            <div key={index} className="lg:px-[8%] flex justify-between mb-12 flex-col md:gap-2 lg:gap-14 md:mb-16 lg:mb-32 lg:last:mb-14 lg:flex-row">
+              {/* 경력 콘텐츠 */}
+              <div className="flex flex-row  justify-between gap-[50px] lg:w-[40%] ">
                 <div className='w-full flex flex-col'>
-                  <h4 className="w-full text-xl lg:text-[33px] leading-[30px] md:text-[22px] lg:w-[180px] font-bold">
+                  {/* 포지션 */}
+                  <h4 className="w-full text-xl lg:text-[33px] leading-8 lg:leading-10 md:text-[22px] lg:w-[180px] font-bold">
                     {experience.position}
                   </h4>
-                  <h5 className="w-full pb-5 text-lg lg:text-[20px] capitalize mt-[10px] text-accent font-normal md:text-[17px]">
+                  {/* 사명 */}
+                  <h5 className="w-full pb-5 text-lg lg:text-lg xl:text-[20px] capitalize mt-[10px] text-accent font-normal md:text-[17px]">
                     {experience.company}
                   </h5>
                 </div>
+                {/* 연도 */}
                 <h3 className="text-xl xs:text-2xl lg:text-[48px] font-medium leading-[45px] md:text-[40px]">
                   {experience.year}
                 </h3>
               </div>
-              <p className="w-full text-sm xs:text-base lg:w-[40%] md:text-[18px] font-light lg:pr-[10%]">
+              {/* 설명 */}
+              <p className="w-full text-sm xs:text-base lg:w-[40%] md:text-[18px] font-light">
                 {experience.description}
               </p>
             </div>
